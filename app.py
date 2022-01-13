ca47fc5af6638825433ef46e848b351b34583821
Michael Miles <michael@miles-me.net>
2022-01-12 19:11:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
 """Author: Michael Miles"""
-
-# \" :Double Quote
-# \' :Single QQuote
-# \\ :Back slash
-# \n :New Line
-
-course = "Python \nProgramming"
-print(course)
+course = "Python Programming"
