2e956c0f903d8dc3f446e628846bae7b290798f3
Michael Miles <michael@miles-me.net>
2022-01-21 04:39:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 """Author: Michael Miles"""
-from msilib.schema import IniFile
-
-
-temperature = 35
-if temperature > 30:
-    print("It's warm")
-    print("Drink Watter")
-print("It's Done")
+age = 12
+message = "Elegible" if age >= 22 else "Not elegible"
+print(message)
